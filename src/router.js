5ba11b928f199eb0dac4eb80307a7b6c18c62781
huangjx <trustfor@sina.cn>
2018-06-04 23:28:15 +0800

    '路由改变'

--- src/router.js
+++ src/router.js
@@ -1,21 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
-import About from './views/About.vue'
+import routes from './views'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home
-    },
-    {
-      path: '/about',
-      name: 'about',
-      component: About
-    }
-  ]
+  routes
 })
---
+++ src/tmp/index.js
@@ -0,0 +1,9 @@
+import MiLogin from './MiLogin.vue'
+
+export default [
+  {
+    path: '/',
+    name: 'login',
+    component: MiLogin
+  }
+]
